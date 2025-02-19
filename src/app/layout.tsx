2193eed6997cb8b59559f4bec110685abe2fd8ca
Pranjal Birla <birlapranjal460@gmail.com>
2025-02-19 18:28:44 +0530

    added some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import { Inter, Poppins } from "next/font/google";
 import type React from "react"; // Import React
-import NavBar from "@/components/NavBar";
-import { Footer } from "@/components/Footer";
+// import NavBar from "@/components/NavBar";
+// import { Footer } from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsAppButton";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -26,11 +26,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${poppins.variable} font-sans text-white min-h-screen w-full relative overflow-x-hidden`}
+        className={`${inter.variable} ${poppins.variable} font-sans text-white min-h-screen w-[98vw] overflow-hidden relative`}
       >
-        <NavBar />
+        {/* <NavBar /> */}
         <main className="max-w-screen mx-auto overflow-hidden">{children}</main>
-        <Footer />
+        {/* <Footer /> */}
         <WhatsAppButton phoneNumber="+919977990520" />
       </body>
     </html>
